cfe3c0135a2c70a3173c78ecb319e7470d78aaff
StaviRare <86742958+StaviRare@users.noreply.github.com>
2021-08-27 00:05:47 +0300

    Folding variables readability fix

--- Assets/GLHF/Folding Variables/Scripts/Example.cs
+++ Assets/GLHF/Folding Variables/Scripts/Example.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 using GLHF.FoldingVariables;
+#pragma warning disable 0414
 
 public class Example : MonoBehaviour
 {
     [SerializeField] private int _serializedVariable = 1;
-    
     [FoldingVariable] [HideInInspector] public int _hiddenFoldedVariable = 2;
-    
     [FoldingVariable] [SerializeField] private string _foldedVariable01 = "This is a folded variable";
-    
     [FoldingVariable] [SerializeField] private GameObject _foldedVariable02 = null;
 }
